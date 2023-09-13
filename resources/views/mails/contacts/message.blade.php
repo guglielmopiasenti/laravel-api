1a13cd17bdb4a87b460bb5787d2e2f05e45575ff
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-13 15:48:03 +0200

    massage.blade created and strucuted

---
+++ resources/views/mails/contacts/message.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Email Subject</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body style="font-family: Arial, sans-serif; background-color: #f5f5f5; padding: 20px;">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center" style="color: #333;">Hello!</h1>
+                <p class="text-center" style="color: #666;">This is a test email message sent to Mailtrap.</p>
+                <div class="text-center">
+                    <a href="#" class="btn btn-primary" style="text-decoration: none; border-radius: 5px;">Visit
+                        Our Website</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
