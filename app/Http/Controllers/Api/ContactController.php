d076e5e68c78a160ce576652a7cdac63601caa2f
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-13 15:54:26 +0200

    controller set

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\ContactMessageMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
     public function message(){
+        $mail = new ContactMessageMail();
+        Mail::to(env('MAIL_TO_ADDRESS'))->send($mail);
+
        return response(null, 204);
     }
 }
